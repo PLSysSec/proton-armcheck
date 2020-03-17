96a60c9c280fd5073050f60503eec8571a4ac6f5
mlfbrown <mlfbrown@stanford.edu>
2020-03-17 12:10:35 -0700

    kildall

--- src/kildall.rs
+++ src/kildall.rs
@@ -19,8 +19,8 @@ pub trait Checkable
 where
     Self: Sized + PartialEq
 {
-    fn meet(fst: Self, snd: Self) -> Self; 
-    fn transfer(fst: Node<Self>) -> Node<Self>; 
+    fn meet(fst: &Self, snd: &Self) -> Self; 
+    fn transfer(fst: &Node<Self>) -> Self; 
 } 
 
 fn getElem<'a, T : Checkable> (node : &Node<T>, store : &'a Store<T>) -> &'a T {
@@ -28,18 +28,34 @@ fn getElem<'a, T : Checkable> (node : &Node<T>, store : &'a Store<T>) -> &'a T {
     return &store.storeMap[id]; 
 }
 
-fn successors<T> (node : &Node<T>) -> &[Node<T>] {
+fn setElem<'a, T : Checkable> (node : &Node<T>, state : T, store : &'a Store<T>) {
+    // let id = &node.nodeId;
+    // store.storeMap[id] = state;
+    panic!();
+}
+
+fn makeAndAddSuccessors<T> (node : &Node<T>, state : T, worklist : &VecDeque<Node<T>>) {
+    // Get the node's successor ids
+    // Add the given state to make new nodes
+    // update the vector to include them
     panic!();
 }
 
 fn kildall<T : Checkable> (mut worklist : VecDeque<Node<T>>, store : Store<T>) -> Store<T> {
     if worklist.len() == 0 { return store; }
     let node = worklist.pop_front().unwrap();
+    // See if the state has reached a fixed point yet
     let incomingState = &node.nodeState;
     let currentState = getElem(&node, &store);
-    if *incomingState == *currentState { return kildall(worklist, store); }
-    let succs = successors(&node);
-    panic!();
+    let newState = T::meet(incomingState, currentState);
+    if newState == *currentState { return kildall(worklist, store); }
+    // Update the information at this node in the state
+    setElem(&node, newState, &store);
+    // Get the information about this node using the transfer function,
+    // and propagate it to all the node's successors
+    let transferredState = T::transfer(&node);
+    makeAndAddSuccessors(&node, transferredState, &worklist);
+    return kildall(worklist, store);
 }
 
 
