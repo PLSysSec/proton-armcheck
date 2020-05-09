29eec2bb34e7f23628455dd9043bc05baf901253
mlfbrown <mlfbrown@stanford.edu>
2020-05-09 15:20:55 -0700

    Checking the match strings

--- invparse/src/Lib.hs
+++ invparse/src/Lib.hs
@@ -4,12 +4,15 @@ module Lib
 import           A32v8
 import           AST
 import           Codegen
-import           Control.Monad (forM_, unless)
+import           Control.Monad (forM, forM_, unless, when)
+import           Data.List     (nub)
 
 someFunc :: IO ()
 someFunc = do
   putStrLn "Checking generated code for correctness"
-  forM_ a32v8instrs $ \(instr, name) -> do
+  -- Get the match strings
+  putStrLn "Getting match strings and checking for 32 bit lengths"
+  strings <- forM a32v8instrs $ \(instr, name) -> do
     let str = bitstring $ genConstantMatchInstr instr
     unless (length str == 32) $ error $ unwords [ "Bad bitstring"
                                                 , name
@@ -19,3 +22,20 @@ someFunc = do
                                                 , show $ length str
                                                 , ")"
                                                 ]
+    return (str, name)
+
+  -- None of the match strings should be the same. We can afford to do this
+  -- slowly and dumbly
+  putStrLn "Checking for overlapping bitpatterns"
+  forM_ strings $ \(s1, name1) ->
+    forM_ strings $ \(s2, name2 ) ->
+      when (name1 /= name2 && s1 == s2) $
+        error $ unwords [ "Overlapping bitstring for instructions"
+                        , name1
+                        , "and"
+                        , name2
+                        , ":\n"
+                        , s1
+                        ]
+
+
