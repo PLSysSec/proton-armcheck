b167dc03b8919ae3aceb8d06ff74c699c369c5d4
mlfbrown <mlfbrown@stanford.edu>
2020-05-09 18:25:48 -0700

    some kind of sane representation?

--- invparse/src/Lib.hs
+++ invparse/src/Lib.hs
@@ -10,33 +10,33 @@ import           Data.List     (nub)
 someFunc :: IO ()
 someFunc = do
   putStrLn "Checking generated code for correctness"
-  -- Get the match strings
-  putStrLn "Getting match strings and checking for 32 bit lengths"
-  strings <- forM a32v8instrs $ \(instr, name) -> do
-    let str = bitstring $ genConstantMatchInstr instr
-    print str
-    unless (length str == 32) $ error $ unwords [ "Bad bitstring"
-                                                , name
-                                                , ":"
-                                                , str
-                                                , "("
-                                                , show $ length str
-                                                , ")"
-                                                ]
-    return (str, name)
+  -- -- Get the match strings
+  -- putStrLn "Getting match strings and checking for 32 bit lengths"
+  -- strings <- forM a32v8instrs $ \(instr, name) -> do
+  --   let str = bitstring $ genConstantMatchInstr instr
+  --   print str
+  --   unless (length str == 32) $ error $ unwords [ "Bad bitstring"
+  --                                               , name
+  --                                               , ":"
+  --                                               , str
+  --                                               , "("
+  --                                               , show $ length str
+  --                                               , ")"
+  --                                               ]
+  --   return (str, name)
 
-  -- None of the match strings should be the same. We can afford to do this
-  -- slowly and dumbly
-  putStrLn "Checking for overlapping bitpatterns"
-  forM_ strings $ \(s1, name1) ->
-    forM_ strings $ \(s2, name2 ) ->
-      when (name1 /= name2 && s1 == s2) $
-        error $ unwords [ "Overlapping bitstring for instructions"
-                        , name1
-                        , "and"
-                        , name2
-                        , ":\n"
-                        , s1
-                        ]
+  -- -- None of the match strings should be the same. We can afford to do this
+  -- -- slowly and dumbly
+  -- putStrLn "Checking for overlapping bitpatterns"
+  -- forM_ strings $ \(s1, name1) ->
+  --   forM_ strings $ \(s2, name2 ) ->
+  --     when (name1 /= name2 && s1 == s2) $
+  --       error $ unwords [ "Overlapping bitstring for instructions"
+  --                       , name1
+  --                       , "and"
+  --                       , name2
+  --                       , ":\n"
+  --                       , s1
+  --                       ]
 
 
