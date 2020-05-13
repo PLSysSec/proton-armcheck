e3d9c0bbafd75c7f2a7fc8575975f2b37a07086c
mlfbrown <mlfbrown@stanford.edu>
2020-05-13 14:32:13 -0700

    add more restrictions and fix existing a32v8 restrictions

--- invparse/src/A32v8.hs
+++ invparse/src/A32v8.hs
@@ -63,6 +63,8 @@ fifteen = c 15
 n    = v 19 16
 t    = v 15 12
 m    = v 3 0
+rt0 = v 12 12
+
 
 dummy = [ constant 31 28 0b1111
         , any 27 0
@@ -76,7 +78,7 @@ ldrimm = [ not 31 28 0b1111
          , constant 20 20 1
          , reg 19 16
          , reg 15 12
-         , any 11 0 -- imm12.
+         , any 11 0
          -- restrictions
          , not' $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
          ]
@@ -88,7 +90,7 @@ ldrlit = [ not 31 28 0b1111
          , any 21 21
          , constant 20 16 0b11111
          , reg 15 12
-         , any 11 0 -- imm12. what do we do here
+         , any 11 0
          -- restrictions
          , (p `eq'` zero) `or'` (w `eq'` one)
          ]
@@ -106,7 +108,8 @@ ldrreg = [ not 31 28 0b1111
          , constant 4 4 0
          , reg 3 0
          -- restrictions
-         , dname "fifteen" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` c 15)
+         , dname "m" $ m `eq'` fifteen
+         , dname "fifteen" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` fifteen)
          , dname "nt" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
          ]
 
@@ -118,10 +121,10 @@ ldrbimm = [ not 31 28 0b1111
           , constant 20 20 1
           , reg 19 16
           , reg 15 12
---          , not 15 12 15
-          , any 11 0 -- imm12. what do we do here
+          , any 11 0
           -- restrictions
-          , ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
+          , dname "t" $ t `eq'` fifteen
+          , dname "nt" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
           ]
 
 ldrblit = [ not 31 28 0b1111
@@ -132,10 +135,10 @@ ldrblit = [ not 31 28 0b1111
           , constant 20 20 1
           , constant 19 16 0b1111
           , reg 15 12
---          , not 15 12 15
-          , any 11 0 -- imm12. what do we do here
+          , any 11 0
           -- restrictions
-          , (p `eq'` zero) `or'` (w `eq'` one)
+          , dname "t" $ t `eq'` fifteen
+          , dname "wback" $ (p `eq'` zero) `or'` (w `eq'` one)
           ]
 
 ldrbreg = [ not 31 28 0b1111
@@ -146,11 +149,13 @@ ldrbreg = [ not 31 28 0b1111
           , constant 20 20 1
           , reg 19 16
           , reg 15 12
-          , any 11 7 -- imm5
-          , any 6 5 -- stype
+          , any 11 7
+          , any 6 5
           , constant 4 4 0
           , reg 3 0
           -- restrictions
+          , dname "t" $ t `eq'` fifteen
+          , dname "m" $ m `eq'` fifteen
           , dname "fifteen" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` c 15)
           , dname "nt" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
           ]
@@ -163,16 +168,15 @@ ldrdimm = [ not 31 28 0b1111
           , constant 20 20 0
           , reg 19 16
           , reg 15 12
---          , not 15 12 0
           , any 11 8 -- imm4H
           , constant 7 4 0b1101
           , any 3 0 -- imm4L
             -- restrictions
---          , not 15 12 0
           , dname "pw" $ (p `eq'` zero) `and'` (w `eq'` one)
           , dname "nt" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
           , dname "nt1" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` (t `add'` one))
           , dname "fifteen" $ (t `add'` one) `eq'` fifteen
+          , dname "r0" $ rt0 `eq'` one
           ]
 
 ldrdlit = [ not 31 28 0b1111
@@ -183,12 +187,12 @@ ldrdlit = [ not 31 28 0b1111
           , constant 20 20 0
           , constant 19 16 0b1111
           , reg 15 12
---          , not 15 12 14
           , any 11 8 -- imm4H
           , constant 7 4 0b1101
           , any 3 0 -- imm4L
             -- restrictions
---          , not 12 12 1 -- not Rt<0> == 1
+          , dname "r0" $ rt0 `eq'` one
+          , dname "t2" $ (t `add'` one) `eq'` fifteen
           ]
 
 ldrdreg = [ not 31 28 0b1111
@@ -203,13 +207,13 @@ ldrdreg = [ not 31 28 0b1111
           , constant 7 4 0b1101
           , reg 3 0
           -- restrictions
---          , not 12 12 1 -- not Rt<0> == 1
+          , dname "r0" $ rt0 `eq'` one
           , dname "pw" $ (p `eq'` zero) `and'` (w `eq'` one)
           , dname "fifteen" $ m `eq'` fifteen
-          , dname "fourteen" $ t `eq'` (c 14)
+          , dname "fourteen" $ (t `add'` one) `eq'` fifteen
           , dname "mt" $ m `eq'` t
           , dname "mt1" $ m `eq'` (t `add'` one)
-          , dname "n15" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` c 15)
+          , dname "n15" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` fifteen)
           , dname "nt" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
           , dname "nt1" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` (t `add'` one))
           ]
@@ -221,15 +225,13 @@ ldrhimm = [ not 31 28 0b1111
           , any 21 21
           , constant 20 20 1
           , reg 19 16
---          , not 19 16 0b1111
           , reg 15 12
---          , not 15 12 0
           , any 11 8 -- imm4H
           , constant 7 4 0b1011
           , any 3 0 -- imm4L
             -- restrictions
           , dname "nt" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
-          , dname "fifteen" $ (t `add'` one) `eq'` fifteen
+          , dname "fifteen" $ t `eq'` fifteen
           ]
 
 ldrhlit = [ not 31 28 0b1111
@@ -260,8 +262,8 @@ ldrhreg = [ not 31 28 0b1111
           , constant 7 4 0b1011
           , reg 3 0
           -- restrictions
---          , not 12 12 1 -- not Rt<0> == 1
-          , dname "tm" $ (t `eq'` fifteen) `or'` (m `eq'` fifteen)
+          , dname "t" $ t `eq'` fifteen
+          , dname "m" $ m `eq'` fifteen
           , dname "fifteen" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` fifteen)
           , dname "nt" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
           ]
@@ -273,14 +275,13 @@ ldrsbimm = [ not 31 28 0b1111
            , any 21 21
            , constant 20 20 1
            , reg 19 16
---           , not 19 16 0b1111
            , reg 15 12
            , any 11 8 -- imm4H
            , constant 7 4 0b1101
            , any 3 0 -- imm4L
            -- restrictions
            , dname "nt" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
-           , dname "fifteen" $ (t `add'` one) `eq'` fifteen
+           , dname "t" $ t `eq'` fifteen
            ]
 
 ldrsblit = [ not 31 28 0b1111
@@ -311,7 +312,8 @@ ldrsbreg = [ not 31 28 0b1111
            , constant 7 4 0b1101
            , reg 3 0
            -- restrictions
-           , dname "fifteen" $ (t `eq'` fifteen) `or'` (m `eq'` fifteen)
+           , dname "t" $ t `eq'` fifteen
+           , dname "m" $ m `eq'` fifteen
            , dname "n" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` fifteen)
            , dname "nt" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
            ]
@@ -323,7 +325,6 @@ ldrshimm = [ not 31 28 0b1111
            , any 21 21
            , constant 20 20 1
            , reg 19 16
---           , not 19 16 0b1111
            , reg 15 12
            , any 11 8 -- imm4H
            , constant 7 4 0b1111
@@ -361,7 +362,8 @@ ldrshreg = [ not 31 28 0b1111
            , constant 7 4 0b1111
            , reg 3 0
            -- restrictions
-           , dname "mt" $ (t `eq'` fifteen) `or'` (m `eq'` fifteen)
+           , dname "t" $ t `eq'` fifteen
+           , dname "m" $ m `eq'` fifteen
            , dname "fifteen" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` fifteen)
            , dname "nt" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
            ]
@@ -443,13 +445,15 @@ strdimm = [ not 31 28 0b1111
           , constant 20 20 0
           , reg 19 16
           , reg 15 12
---          , not 15 12 0
           , any 11 8 -- imm4H
           , constant 7 4 0b1111
           , any 3 0 -- imm4L
             -- restrictions
-          , dname "w" $ (p `eq'` zero) `and'` (w `eq'` one)
-          , dname "n15" $ n `eq'` fifteen
+          , dname "pw" $ (p `eq'` zero) `and'` (w `eq'` one)
+          , dname "n15"$ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` fifteen)
+          , dname "nt"$ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
+          , dname "nt2"$ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` (t `add'` one))
+          , dname "t2" $ (t `add'` one) `eq'` fifteen
           ]
 
 strdreg = [ not 31 28 0b1111
@@ -464,9 +468,12 @@ strdreg = [ not 31 28 0b1111
           , constant 7 4 0b1111
           , reg 3 0
           -- restrictions
-          , dname "pw" $ (p `eq'` zero) `or'` (w `eq'` one)
-          , dname "m15" $ m `eq'` fifteen
-          , dname "fifteen" $ (t `add'` one) `eq'` fifteen
+          , dname "pw" $ (p `eq'` zero) `and'` (w `eq'` one)
+          , dname "t2" $ (t `add'` one) `eq'` fifteen
+          , dname "m" $ m `eq'` fifteen
+          , dname "n15"$ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` fifteen)
+          , dname "nt"$ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
+          , dname "nt2"$ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` (t `add'` one))
           ]
 
 strhimm = [ not 31 28 0b1111
@@ -498,7 +505,8 @@ strhreg = [ not 31 28 0b1111
           , constant 7 4 0b1011
           , reg 3 0
           -- restrictions
-          , dname "mt" $ (t `eq'` fifteen) `or'` (m `eq'` fifteen)
+          , dname "t" $ t `eq'` fifteen
+          , dname "m" $ m `eq'` fifteen
           , dname "n15" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` fifteen)
           , dname "nt" $ ((p `eq'` zero) `or'` (w `eq'` one)) `and'` (n `eq'` t)
           ]
@@ -513,10 +521,19 @@ atomicLoad c = [ not 31 28 0b1111
                , any 3 0
                ]
 
-ldrex = atomicLoad 0b00011001
-ldrexb = atomicLoad 0b00011101
-ldrexd = atomicLoad 0b00011011 -- and Rt<0> == 1, Rt in range 15-12
-ldrexh = atomicLoad 0b00011111
+ldrex = atomicLoad 0b00011001 ++ [ dname "t" $ t `eq'` fifteen
+                                 , dname "n" $ n `eq'` fifteen
+                                 ]
+ldrexb = atomicLoad 0b00011101 ++ [ dname "t" $ t `eq'` fifteen
+                                  , dname "n" $ n `eq'` fifteen
+                                  ]
+ldrexd = atomicLoad 0b00011011 ++ [ dname "rt0" $ rt0 `eq'` one
+                                  , dname "t2" $ (t `add'` one) `eq'` fifteen
+                                  , dname "n" $ n `eq'` fifteen
+                                  ]
+ldrexh = atomicLoad 0b00011111 ++ [ dname "t" $ t `eq'` fifteen
+                                  , dname "n" $ n `eq'` fifteen
+                                  ]
 
 -- Atomic stores
 atomicStore c = [ not 31 28 0b1111
@@ -526,14 +543,39 @@ atomicStore c = [ not 31 28 0b1111
                 , any 11 10
                 , constant 9 4 0b111001
                 , not15 3 0
-                -- , neq 19 16 15 12
-                -- , neq 19 16 3 0
                 ]
 
-strex  = atomicStore 0b00011000
-strexb = atomicStore 0b00011100
-strexd = atomicStore 0b00011010 -- and Rt<0> == 1, Rt in in range 3-0
-strexh = atomicStore 0b00011110
+rn = v 19 16
+rd = v 15 12
+rt = v 3 0
+rt0' = v 0 0
+
+strex  = atomicStore 0b00011000 ++ [ dname "d" $ rd `eq'` fifteen
+                                   , dname "t" $ rt `eq'` fifteen
+                                   , dname "n" $ rn `eq'` fifteen
+                                   , dname "dn" $ rd `eq'` rn
+                                   , dname "dt" $ rd `eq'` rt
+                                   ]
+strexb = atomicStore 0b00011100 ++ [ dname "d" $ rd `eq'` fifteen
+                                   , dname "t" $ rt `eq'` fifteen
+                                   , dname "n" $ rn `eq'` fifteen
+                                   , dname "dn" $ rd `eq'` rn
+                                   , dname "dt" $ rd `eq'` rt
+                                   ]
+strexd = atomicStore 0b00011010 ++ [ dname "d" $ rd `eq'` fifteen
+                                   , dname "rt0" $ rt0' `eq'` one
+                                   , dname "t2" $ (rt `add'` one) `eq'` fifteen
+                                   , dname "n" $ rn `eq'` fifteen
+                                   , dname "dn" $ rd `eq'` rn
+                                   , dname "dt" $ rd `eq'` rt
+                                   , dname "dt2" $ rd `eq'` (rt `add'` one)
+                                   ]
+strexh = atomicStore 0b00011110 ++ [ dname "d" $ rd `eq'` fifteen
+                                   , dname "t" $ rt `eq'` fifteen
+                                   , dname "n" $ rn `eq'` fifteen
+                                   , dname "dn" $ rd `eq'` rn
+                                   , dname "dt" $ rd `eq'` rt
+                                   ]
 
 -- Helpers:
 
