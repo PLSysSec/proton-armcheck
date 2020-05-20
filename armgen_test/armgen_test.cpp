2c3e11bf2fe965ff7075356379725ccb9a052563
kwantam <kwantam@gmail.com>
2020-05-19 18:38:46 -0700

    armgen, now with more oomph

---
+++ armgen_test/armgen_test.cpp
@@ -0,0 +1,48 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <optional>
+#include <set>
+
+using namespace std;
+
+optional<int32_t> get_jump_target(uint32_t instr);
+set<size_t> get_all_targets(const uint32_t *buf, size_t len);
+extern "C" bool proton_armcheck_check_buffer(const uint32_t *buf, size_t len,
+											 size_t *fail_idx,
+											 const char **fail_instr_name,
+											 const char **fail_test_name);
+
+int main(int argc, char **argv) {
+    int fd = [&] {
+        if (argc < 2) {
+            return open("fuck_0000.out", O_RDONLY);
+        } else {
+            return open(argv[1], O_RDONLY);
+        }
+    }();
+    if (fd < 0) {
+        perror("Could not open file");
+        return -1;
+    }
+	size_t len = lseek(fd, 0, SEEK_END);
+    uint32_t *buf = (uint32_t *)mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
+
+    size_t fail_idx;
+    const char *fail_instr_name;
+    const char *fail_test_name;
+    if (!proton_armcheck_check_buffer(buf, len / 4, &fail_idx, &fail_instr_name, &fail_test_name)) {
+        printf("failed: %s (%s) at %lx\n", fail_instr_name, fail_test_name, fail_idx);
+        return -2;
+    }
+
+    return 0;
+}
